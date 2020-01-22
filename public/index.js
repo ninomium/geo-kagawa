5f64fc7bc64f5cf8378e5bff91430c0d531e6322
Kota Ninomiya <uv.ninomiya@gmail.com>
2020-01-22 12:22:07 +0900

    Add comment  and fix constant

--- public/index.js
+++ public/index.js
@@ -1,11 +1,17 @@
-API = 'http://www.finds.jp/ws/rgeocode.php'
+const API_URL = 'http://www.finds.jp/ws/rgeocode.php'
 
 document.addEventListener('DOMContentLoaded', () => {
     const message_id = document.getElementById('message')
+
+    // 位置情報取得対応かチェック
     if(navigator.geolocation){
-        getCurrentPosition().then(postion => {
+        // 現在位置をPromiseとして取得
+        getCurrentPositionAsPromise().then(postion => {
+            // 逆ジオコーディングにかける
+            console.log('Got Position')
             getPrefecture(postion.coords.latitude, postion.coords.longitude).then(json => {
                 const prefecuture = json.result.prefecture.pname
+                // 香川県なら県のホームページへ
                 if(prefecuture == '香川県'){
                     location.href = 'https://www.pref.kagawa.lg.jp/'
                 }else {
@@ -24,14 +30,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-function getCurrentPosition(){
+function getCurrentPositionAsPromise(){
     return new Promise((resolve, reject) =>{
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
 }
 
 function getPrefecture(latitude, longitude){
-    return fetch(`${API}?json&lat=${latitude}&lon=${longitude}`,{
+    return fetch(`${API_URL}?json&lat=${latitude}&lon=${longitude}`,{
         mode: 'cors'
     }).then(response => {
         if(response.ok){
