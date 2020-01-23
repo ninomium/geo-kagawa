77a60fb0025fd01853e4f1ec4b986beb3c0de874
Kota Ninomiya <uv.ninomiya@gmail.com>
2020-01-23 23:14:49 +0900

    Change for async/wait

--- public/index.js
+++ public/index.js
@@ -1,42 +1,39 @@
 const API_URL = 'http://www.finds.jp/ws/rgeocode.php'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const message_id = document.getElementById('message')
 
     // 位置情報取得対応かチェック
     if(navigator.geolocation){
-        // 現在位置をPromiseとして取得
-        getCurrentPositionAsPromise().then(postion => {
-            // 逆ジオコーディングにかける
-            console.log('Got Position')
-            getPrefecture(postion.coords.latitude, postion.coords.longitude).then(json => {
-                const prefecuture = json.result.prefecture.pname
-                // 香川県なら県のホームページへ
-                if(prefecuture == '香川県'){
-                    location.href = 'https://www.pref.kagawa.lg.jp/'
-                }else {
-                    message_id.innerHTML = 'ようこそ'
-                }
-            }, reject => {
-                console.log(reject)
-                message_id.innerHTML = '都道府県を特定できませんでした'
-            })
-        }, reject => {
+        // 現在位置をPromiseとして取得  - (1)
+        const postion = await getCurrentPositionAsPromise().catch(error => {
             console.log(reject)
             message_id.innerHTML = '現在位置を取得できませんでした'
         })
+        // 逆ジオコーディングにかける - (2)
+        const json = await getPrefecture(postion.coords.latitude, postion.coords.longitude).catch(error => {
+            console.log(reject)
+            message_id.innerHTML = '都道府県を特定できませんでした'
+        })
+        const prefecuture = json.result.prefecture.pname
+        // 香川県なら県のホームページへ
+        if (prefecuture === '香川県') {
+            location.href = 'https://www.pref.kagawa.lg.jp/'
+        } else {
+            message_id.innerHTML = 'ようこそ'
+        }
     }else {
         message_id.innerHTML = '位置情報取得を許可してください'
     }
 })
 
-function getCurrentPositionAsPromise(){
+async function getCurrentPositionAsPromise(){
     return new Promise((resolve, reject) =>{
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
 }
 
-function getPrefecture(latitude, longitude){
+async function getPrefecture(latitude, longitude){
     return fetch(`${API_URL}?json&lat=${latitude}&lon=${longitude}`,{
         mode: 'cors'
     }).then(response => {
