2091db643ae2812181f03ba8c2e7d3fcf3333796
Kota Ninomiya <uv.ninomiya@gmail.com>
2020-01-27 18:08:50 +0900

    Fix mistakes and add on README

--- public/index.js
+++ public/index.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     const message_id = document.getElementById('message')
 
     // 位置情報取得対応かチェック
-    if(navigator.geolocation){
+    if (navigator.geolocation) {
         // 現在位置をPromiseとして取得  - (1)
         const postion = await getCurrentPositionAsPromise().catch(error => {
-            console.log(reject)
+            console.log(error)
             message_id.innerHTML = '現在位置を取得できませんでした'
         })
         // 逆ジオコーディングにかける - (2)
         const json = await getPrefecture(postion.coords.latitude, postion.coords.longitude).catch(error => {
-            console.log(reject)
+            console.log(error)
             message_id.innerHTML = '都道府県を特定できませんでした'
         })
         const prefecuture = json.result.prefecture.pname
@@ -22,25 +22,24 @@ document.addEventListener('DOMContentLoaded', async () => {
         } else {
             message_id.innerHTML = 'ようこそ'
         }
-    }else {
+    } else {
         message_id.innerHTML = '位置情報取得を許可してください'
     }
 })
 
-async function getCurrentPositionAsPromise(){
-    return new Promise((resolve, reject) =>{
+async function getCurrentPositionAsPromise() {
+    return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
 }
 
-async function getPrefecture(latitude, longitude){
-    return fetch(`${API_URL}?json&lat=${latitude}&lon=${longitude}`,{
+async function getPrefecture(latitude, longitude) {
+    const response = await fetch(`${API_URL}?json&lat=${latitude}&lon=${longitude}`,{
         mode: 'cors'
-    }).then(response => {
-        if(response.ok){
-            return response.json()
-        }else {
-            return Promise.reject(new Error(`${response.status}: ${response.statusText}`))
-        }
     })
+    if (response.ok) {
+        return response.json()
+    } else {
+        throw new Error(`${response.status}: ${response.statusText}`)
+    }
 }
